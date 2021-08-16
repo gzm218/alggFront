287962cec6d18f5966c1d74e968f362ec61627ff
阿勒锅锅 <57820381+gzm218@users.noreply.github.com>
2021-08-16 18:02:48 +0800

    Add files via upload
    自定义事件+事件总线  vuex遇到点问题 有会的兄弟点拨一下

---
+++ busAndCustom/components/List.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="todo-main">
+    <Todos v-for="todoObj in todos" :key="todoObj.id" :todos="todoObj"></Todos>
+  </ul>
+</template>
+
+<script>
+import Todos from './Todos.vue'
+export default {
+  name: 'List',
+  components: { Todos },
+  props: ['todos'],
+}
+</script>
+
+<style>
+/*List*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
